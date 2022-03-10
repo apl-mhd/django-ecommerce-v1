79788ba525ad6a0678bb12ce38e184944e8809bf
Apel Mahmud <44221242+apl-mhd@users.noreply.github.com>
2022-03-10 14:02:43 +0600

    admin registered

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,18 @@
+from itertools import product
+from logging import addLevelName
 from django.contrib import admin
 
 from .models import Category, Product
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name', )}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'slug', 'price', 'in_stock', 'created', 'updated']
+    list_filter = ['in_stock', 'is_active']
+    list_editable = ['price', 'in_stock']
+    prepopulated_fields = {'slug': ('title',)}
