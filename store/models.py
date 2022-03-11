34269de2fe0deea2050e47f1bbb5d7ee16d1163b
Apel Mahmud <44221242+apl-mhd@users.noreply.github.com>
2022-03-11 22:23:22 +0600

    dynamic url

--- store/models.py
+++ store/models.py
@@ -3,6 +3,7 @@ from turtle import title
 from unicodedata import category
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 # Create your models here.
@@ -13,6 +14,10 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'categories'
+    
+    def get_absolute_url(self):
+        return reverse('store:category_list', args=[self.slug])
+    
     
     def __str__(self):
         return self.name
@@ -35,6 +40,10 @@ class Product(models.Model):
     class Meta:
         verbose_name_plural = 'Products'
         ordering = ('-created',)
+
+    def get_absolute_url(self):
+        return reverse('store:product_detail', args=[self.slug ])
+        
 
     def __str__(self):
         return self.title
--- store/urls.py
+++ store/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     
     path('', views.all_products, name='all_products'),
     path('item/<slug:slug>', views.product_detail, name='product_detail'),
+    path('search/<slug:category_slug>/', views.category_list, name='category_list'),
 ]
